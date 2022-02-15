7ad1bd6319255494fe5c1bfc5f4fb5143ca07f92
ishfaq <you@example.com>
2022-02-15 21:14:51 +0500

    latest hote cards

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import { HotelDetail } from './HotelDetail';
 const Resturants=[
   {id:1,Name:"Crados", Rating:1,Cousions:"ABC"},
   {id:2,Name:"Bundu Khan", Rating:2,Cousions:"DEF"},
-  {id:3,Name:"Zakir Tikkah", Rating:3,Cousions:"GHI"},
-  {id:4,Name:"The GRill", Rating:4,Cousions:"Gpp"}
+  // {id:3,Name:"Zakir Tikkah", Rating:3,Cousions:"GHI"},
+  // {id:4,Name:"The GRill", Rating:4,Cousions:"Gpp"},
+  // {id:5,Name:"Salt n pepper", Rating:5,Cousions:"LLL"}
 ]
 function App() {
   return (
--- src/Restaurants.js
+++ src/Restaurants.js
@@ -1,56 +1,47 @@
 import React from 'react'
 import './Resturants.css';
-
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import {Link} from 'react-router-dom'
-
 export const Restaurants = (props) => {
-    const [selected, setSelected] = useState("")
-    const [toggleCard,setToggleCard]= useState(false)
-    const myRef = useRef(null);
-    const changeColor = (id) => {
-        setToggleCard(!toggleCard)
-        setSelected(id)
-       
-       
-       
-    }
-    return (
+    const [Appstate, changeState] = useState({
+        activeObject: null,
+        objects: props.restaurants
+    })
+    console.log(Appstate.objects)
+    const toggleActive = (index) => {
+        changeState({ ...Appstate, activeObject: Appstate.objects[index] })
+        console.log(Appstate.objects[index])
+        console.log(Appstate.activeObject)
+        }
+       return (
         <div>
-
-            <div className="main d-grid gap-4"  >{
-                props.restaurants.map((hotel) => {
-const {id}=hotel
-                    return (
-                        <>
-                    
-                            <div className="item " ref={myRef} key={id}
-                                style={{ backgroundColor: selected === id 
-                                    && toggleCard
-                                    ? "red" : "" }}
-                                onClick={() => changeColor(hotel.id)}
-                            >
-
-
-
-                        
-
-                                id:{hotel.id}<br />
+             <div className="main d-grid gap-4"  >{
+                   Appstate.objects.map((hotel, index) => {
+                       return (
+                           <>
+                             <div className="item" key={index}
+                                   style={{
+                                       background: Appstate.objects[index] === Appstate.activeObject ? 'red' : ''
+                                   }}
+                                   onClick={() => toggleActive(index)}
+                               >
+                                   id:{hotel.id}<br />
                         Name:{hotel.Name}<br />
+                        index:{index}
                         Cousions:{hotel.Cousions}<br />
                         Rating: {hotel.Rating}
-                      <br/>
-                        <Link to={`/HotelDetail/${hotel.id}/${hotel.Name}`}>View</Link> 
-                      
-                            </div>
-                            
-                           
+                                   <br />
+                                   <Link to={`/HotelDetail/${hotel.id}/${hotel.Name}`}>View</Link>
 
-                        </>
-                    )
-                })
-            }
-            </div>
+                               </div>
+
+                               <h2>index:{index}</h2>
+
+                           </>
+                       )
+                   })
+               }
+               </div>
 
         </div>
     )

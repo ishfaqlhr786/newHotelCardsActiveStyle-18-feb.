2a1065dab465f40ace3d38044ae5222490c58d48
ishfaq <you@example.com>
2022-02-15 15:30:53 +0500

    resturants cards made

--- src/Restaurants.js
+++ src/Restaurants.js
@@ -6,21 +6,26 @@ import { useState, useRef } from 'react'
 
 export const Restaurants = (props) => {
     const [selected, setSelected] = useState("")
+    const [toggleCard,setToggleCard]= useState(false)
     const myRef = useRef(null);
     const changeColor = (id) => {
+        setToggleCard(!toggleCard)
         setSelected(id)
+       
     }
     return (
         <div>
 
             <div className="main"  >{
-                props.restaurants.map((hotel, i) => {
+                props.restaurants.map((hotel) => {
 
                     return (
                         <>
 
                             <div className="item " ref={myRef} key={hotel.id}
-                                style={{ backgroundColor: selected === hotel.id ? "red" : "" }}
+                                style={{ backgroundColor: selected === hotel.id 
+                                    && toggleCard
+                                    ? "red" : "" }}
                                 onClick={() => changeColor(hotel.id)}
                             >
 
